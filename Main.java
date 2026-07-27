f1c3056867699fed0f702ce83bdfc833de9008d1
harshini3001 <e24ai067@shanmugha.edu.in>
2026-07-27 22:01:32 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main{
+    static int factorial(int n){
+        int fact=1;
+        for(int i=1;i<=n;i++){
+            fact=fact*i;
+        }
+        return fact;
+    }
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        System.out.print(factorial(n));
+    }
+    
+}
